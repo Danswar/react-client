70397ce5b924aa187cd54c400300d221a779ab42
Danswar <danswarrior1@gmail.com>
2020-03-02 09:24:32 -0300

    react-router implemented

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import './App.css';
 
 import { Provider } from 'react-redux';
 import store from "./store";
 
-import Home from "./components/Home";
+import Main from "./components/Main";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-        <Home/>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main/>
+        </div>
+      </BrowserRouter>
     </Provider>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { useSelector } from "react-redux";
+import { Switch, Route, Link } from "react-router-dom";
 
 function Home() {
 
@@ -10,6 +10,15 @@ function Home() {
     <div>
       <h1>Bienvenido, happy hack!!</h1>
       <h2>Redux: {reduxTest.msg}</h2>
+      <h2>React Router: <Link to="/">to root</Link> - <Link to="/other">to other</Link> </h2>
+      <Switch>
+        <Route exact path="/">
+          Hello from router from root page
+        </Route>
+        <Route exact path="/other">
+          Hello from router from other page
+        </Route>
+      </Switch>
     </div>
   );
 }
