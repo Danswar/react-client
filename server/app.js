1b03ef58c4f4fa4121d3c9f1e754cbc1e0687c15
Danswar <48102227+Danswar@users.noreply.github.com>
2020-03-05 14:37:46 -0300

    cors and body-parser installed

--- server/app.js
+++ server/app.js
@@ -1,12 +1,18 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
 const app = express();
 
 //Middlewares
+app.use(cors());
+app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 //Routes
+app.options("*", cors()); //Allow prefligth request
+
 const router = express.Router();
 router.get("/", (req, res) => {
   console.log("router1");
