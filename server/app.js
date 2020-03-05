b9bbcb6d7ece8f8f97695fbc44c0e8da4a48a584
Danswar <48102227+Danswar@users.noreply.github.com>
2020-03-05 15:22:30 -0300

    database setup

--- server/app.js
+++ server/app.js
@@ -3,12 +3,21 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+
 const app = express();
 
 //Middlewares
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan("dev"));
+
+//DB Connection
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB connect..."))
+  .catch(err => console.log(err));
 
 //Routes
 app.options("*", cors()); //Allow prefligth request
---
+++ server/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //MongoDB
+  mongoURI:
+    "mongodb+srv://root:Os9EciWV8Klr3vjc@cluster0-33xqs.mongodb.net/test?retryWrites=true&w=majority"
+};
