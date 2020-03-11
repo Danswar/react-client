bd85101fc5f1c032ea781ddc2686a4d9a3021e0c
Danswar <Danswarrior1@gmail.com>
2020-03-10 22:02:37 -0300

    basic router config

---
+++ client/src/components/Boot/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Boot = () => {
+  return (
+    <div
+      style={{
+        height: "100vh"
+      }}
+      className="d-flex justify-content-center align-items-center"
+    >
+      <img
+        src="https://via.placeholder.com/200"
+        alt="logo"
+        style={{
+          borderRadius: "50%",
+          boxShadow: "60px 60px 100px 0px rgba(241,241,241,1)"
+        }}
+      />
+    </div>
+  );
+};
+
+export default Boot;
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Boot from "./Boot";
+import Signin from "./Signin";
 import Dashboard from "./Dashboard";
+import NoMatch from "./NoMatch";
 
 function Main() {
   return (
     <div>
-      <Dashboard />
+      <Switch>
+        <Route path="/" exact={true} component={Boot} />
+        <Route path="/signin" exact={true} component={Signin} />
+        <Route path="/dashboard" exact={true} component={Dashboard} />
+        <Route path="*" component={NoMatch} />
+      </Switch>
     </div>
   );
 }
